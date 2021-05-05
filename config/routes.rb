3c8d389bb546b880495c6de85d17da640e9d21c8
anais-mcho <anais.mancho@mmibordeaux.com>
2021-05-05 14:00:13 +0200

    css edit et coche

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   namespace :recipes_list do
     resources :todo_items
   end
+
+  root "recipes_lists#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
